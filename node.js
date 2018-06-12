ccd033420c083adba35d44ec32fbec5a11f55a6f
Hugo Zapata <hugozap@gmail.com>
2018-06-12 18:38:26 -0500

    set env var at top

--- node.js
+++ node.js
@@ -1,6 +1,7 @@
-var Gun = require('gun')
-var createGunTransport = require('./gun-transport')
 var process = require('process')
 process.env.GUN_ENV = 'production'
+var Gun = require('gun')
+var createGunTransport = require('./gun-transport')
+
 
 module.exports = createGunTransport(Gun)
