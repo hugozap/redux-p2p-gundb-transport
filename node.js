9ac50944aaa7765768ac18ddc3e5df49a5539372
Hugo Zapata <hugozap@gmail.com>
2018-06-12 18:33:53 -0500

    set GUN_ENV to production in node mode, to remove network messages

--- node.js
+++ node.js
@@ -1,4 +1,6 @@
 var Gun = require('gun')
 var createGunTransport = require('./gun-transport')
+var process = require('process')
+process.env.GUN_ENV = 'production'
 
 module.exports = createGunTransport(Gun)
